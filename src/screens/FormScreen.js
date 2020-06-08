25ef5f9def643ed5d33c4f35faa1a83d7a361c1d
Niels Koster <n.koster@portavita.eu>
2020-06-08 23:50:38 +0200

    auth / youtube

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect, useContext } from 'react'
+import { View } from 'react-native'
+import { Input, Button } from 'react-native-elements'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const FormScreen = _ => {
+    const { tryLocalSignin } = useContext(AuthContext)
+    useEffect(_ => { tryLocalSignin() }, [])
+    const [youtube, setYoutube] = useState('')
     return (
         <View>
-            <Text>FormScreen</Text>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{ width: '70%'}}>
+                    <Input
+                        style={{flex: 1}}
+                        value={youtube}
+                        onChangeText={setYoutube}
+                        inputStyle={{}}
+                        placeholder='enter a youtube key here'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        label='YOUTUBE'
+                    />
+                </View>
+                <View style={{ width: '30%'}}>
+                    <Button
+                        style={{flex: 1}}
+                        title='send new youtube key'
+                        onPress={_ => {}}
+                    />
+                </View>
+            </View>
         </View>
     )
+}
+
+FormScreen.navigationOptions = {
+    headerShown: false
 }
 
 export default FormScreen
