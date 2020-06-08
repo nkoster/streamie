e28aa17ed42ed87f7625a40f8c8d074d08b3b8ed
Niels Koster <n.koster@portavita.eu>
2020-06-08 23:51:26 +0200

    tryLocalSignin

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { NavigationEvents } from 'react-navigation'
 import { Context as AuthContext } from '../context/AuthContext'
 import AuthForm from '../components/AuthForm'
 
-const SigninScreen = ({ navigation }) => {
+const SigninScreen = _ => {
+    const { tryLocalSignin } = useContext(AuthContext)
+    useEffect(_ => { tryLocalSignin() }, [])
     const { state, signin, clearErrorMessage } = useContext(AuthContext)
     return (
         <View style={styles.container}>
