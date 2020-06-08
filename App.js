46c37aa5edfc87b7fe5c460a97a46764cf07c3ed
Niels Koster <n.koster@portavita.eu>
2020-06-08 23:50:04 +0200

    cleanup

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import InitialScreen from './src/screens/InitialScreen'
 import SigninScreen from './src/screens/SigninScreen'
 import FormScreen from './src/screens/FormScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
 import { setNavigator } from './src/navigationRef'
 
 const stackNavigator = createStackNavigator({
-  // InitialScreen,
   SigninScreen, FormScreen
 })
 
