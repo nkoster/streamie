9b1a4af2467d7a0b7d97a1f702a9380a22aea8d0
Niels Koster <n.koster@portavita.eu>
2020-06-10 23:42:47 +0200

    basic form + checkboxes

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,35 +1,75 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { View } from 'react-native'
-import { Input, Button } from 'react-native-elements'
+import { View, Text } from 'react-native'
+import { Input, Button, CheckBox } from 'react-native-elements'
 import { Context as AuthContext } from '../context/AuthContext'
 
 const FormScreen = _ => {
     const { tryLocalSignin } = useContext(AuthContext)
     useEffect(_ => { tryLocalSignin() }, [])
     const [youtube, setYoutube] = useState('')
+    const [youtubeActive, setYoutubeActive] = useState(true)
+    const [twitch, setTwitch] = useState('')
+    const [twitchActive, setTwitchActive] = useState(true)
+    const [facebook, setFacebook] = useState('')
+    const [facebookActive, setFacebookActive] = useState(true)
     return (
         <View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-                <View style={{ width: '70%'}}>
-                    <Input
-                        style={{flex: 1}}
-                        value={youtube}
-                        onChangeText={setYoutube}
-                        inputStyle={{}}
-                        placeholder='enter a youtube key here'
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        label='YOUTUBE'
-                    />
-                </View>
-                <View style={{ width: '30%'}}>
-                    <Button
-                        style={{flex: 1}}
-                        title='send new youtube key'
-                        onPress={_ => {}}
-                    />
-                </View>
+            <Text style={{margin:10, fontSize: 20}}>active</Text>
+            <View style={{flex:1, flexDirection: 'row'}}>
+                <CheckBox
+                    onPress={_ => setYoutubeActive(!youtubeActive)}
+                    checked={youtubeActive}
+                    checkedColor='#5090ff'
+                    iconLeft
+                />
+                <Input
+                    value={youtube}
+                    onChangeText={setYoutube}
+                    inputStyle={{}}
+                    placeholder='enter a youtube key here'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    label='YOUTUBE'
+                />
             </View>
+            <View style={{flex:1, flexDirection: 'row'}}>
+                <CheckBox
+                    onPress={_ => setTwitchActive(!twitchActive)}
+                    checked={twitchActive}
+                    checkedColor='#5090ff'
+                    iconLeft
+                />
+                <Input
+                    value={twitch}
+                    onChangeText={setTwitch}
+                    inputStyle={{}}
+                    placeholder='enter a youtube key here'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    label='TWITCH'
+                />
+            </View>
+            <View style={{flex:1, flexDirection: 'row'}}>
+                <CheckBox
+                    onPress={_ => setFacebookActive(!facebookActive)}
+                    checked={facebookActive}
+                    checkedColor='#5090ff'
+                    iconLeft
+                />
+                <Input
+                    value={facebook}
+                    onChangeText={setFacebook}
+                    inputStyle={{}}
+                    placeholder='enter a youtube key here'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    label='FACEBOOK'
+                />
+            </View>
+            <Button
+                title='submit your changes'
+                onPress={_ => {}}
+            />
         </View>
     )
 }
