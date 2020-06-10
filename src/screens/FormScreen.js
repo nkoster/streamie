baeed61c8fa368d7f57ce4285d0854cc0fd0f5aa
Niels Koster <n.koster@portavita.eu>
2020-06-11 00:37:06 +0200

    more form stuff

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { Input, Button, CheckBox } from 'react-native-elements'
 import { Context as AuthContext } from '../context/AuthContext'
 
@@ -13,58 +13,72 @@ const FormScreen = _ => {
     const [facebook, setFacebook] = useState('')
     const [facebookActive, setFacebookActive] = useState(true)
     return (
-        <View>
-            <Text style={{margin:10, fontSize: 20}}>active</Text>
+        <View style={styles.container}>
+            <Button
+                style={{marginBottom: 50}}
+                title='get current keys'
+                onPress={_ => {}}
+            />
+            <Text style={{margin:10, fontSize: 14, color: '#666', fontWeight: 'bold'}}>active</Text>
             <View style={{flex:1, flexDirection: 'row'}}>
-                <CheckBox
-                    onPress={_ => setYoutubeActive(!youtubeActive)}
-                    checked={youtubeActive}
-                    checkedColor='#5090ff'
-                    iconLeft
-                />
-                <Input
-                    value={youtube}
-                    onChangeText={setYoutube}
-                    inputStyle={{}}
-                    placeholder='enter a youtube key here'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    label='YOUTUBE'
-                />
+                <View style={{flex: 1, maxWidth: 50}}>
+                    <CheckBox
+                        onPress={_ => setYoutubeActive(!youtubeActive)}
+                        checked={youtubeActive}
+                        checkedColor='#5090ff'
+                        iconLeft
+                    />
+                </View>
+                <View style={{flex: 1, alignSelf: 'stretch'}}>
+                    <Input
+                        value={youtube}
+                        onChangeText={setYoutube}
+                        placeholder='enter a youtube key here'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        label='YOUTUBE'
+                    />
+                </View>
             </View>
             <View style={{flex:1, flexDirection: 'row'}}>
-                <CheckBox
-                    onPress={_ => setTwitchActive(!twitchActive)}
-                    checked={twitchActive}
-                    checkedColor='#5090ff'
-                    iconLeft
-                />
-                <Input
-                    value={twitch}
-                    onChangeText={setTwitch}
-                    inputStyle={{}}
-                    placeholder='enter a youtube key here'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    label='TWITCH'
-                />
+                <View style={{flex: 1, maxWidth: 50}}>
+                    <CheckBox
+                        onPress={_ => setTwitchActive(!twitchActive)}
+                        checked={twitchActive}
+                        checkedColor='#5090ff'
+                        iconLeft
+                    />
+                </View>
+                <View style={{flex: 1, alignSelf: 'stretch'}}>
+                    <Input
+                        value={twitch}
+                        onChangeText={setTwitch}
+                        placeholder='enter a youtube key here'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        label='TWITCH'
+                    />
+                </View>
             </View>
             <View style={{flex:1, flexDirection: 'row'}}>
-                <CheckBox
-                    onPress={_ => setFacebookActive(!facebookActive)}
-                    checked={facebookActive}
-                    checkedColor='#5090ff'
-                    iconLeft
-                />
-                <Input
-                    value={facebook}
-                    onChangeText={setFacebook}
-                    inputStyle={{}}
-                    placeholder='enter a youtube key here'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    label='FACEBOOK'
-                />
+                <View style={{flex: 1, maxWidth: 50}}>
+                    <CheckBox
+                        onPress={_ => setFacebookActive(!facebookActive)}
+                        checked={facebookActive}
+                        checkedColor='#5090ff'
+                        iconLeft
+                    />
+                </View>
+                <View style={{flex: 1, alignSelf: 'stretch'}}>
+                    <Input
+                        value={facebook}
+                        onChangeText={setFacebook}
+                        placeholder='enter a youtube key here'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        label='FACEBOOK'
+                    />
+                </View>
             </View>
             <Button
                 title='submit your changes'
@@ -73,6 +87,16 @@ const FormScreen = _ => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        marginTop: 130,
+        marginBottom: 130
+    }
+})
 
 FormScreen.navigationOptions = {
     headerShown: false
