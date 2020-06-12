3729d0d7a6639624783c2ade17e2ad97150a6571
Niels Koster <n.koster@portavita.eu>
2020-06-12 22:22:23 +0200

    initial, SaveScreen

---
+++ src/screens/SaveScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
+
+const SaveScreen = ({navigation}) => {
+    return (
+        <View>
+            <NavigationEvents
+                onWillFocus={_ => {
+                    setTimeout(_ => navigation.navigate('FormScreen'), 2000)
+                }}
+            />
+            <Text style={styles.text}>streamie updated!</Text>
+        </View>
+    )
+}
+
+SaveScreen.navigationOptions = {
+    headerShown: false
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30,
+        alignSelf: 'center',
+        marginTop: 150
+    }
+})
+
+export default SaveScreen
