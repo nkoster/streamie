baf4b65c32290f13ca4807f0667a9d9002a3b6b5
Niels Koster <n.koster@portavita.eu>
2020-06-14 18:09:35 +0200

    styling / show streamUser

--- src/screens/SaveScreen.js
+++ src/screens/SaveScreen.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { NavigationEvents } from 'react-navigation'
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 const SaveScreen = ({navigation}) => {
+    const streamUser = navigation.getParam('streamUser')
     return (
         <View>
             <NavigationEvents
                 onWillFocus={_ => {
-                    setTimeout(_ => navigation.navigate('FormScreen'), 1500)
+                    setTimeout(_ => navigation.navigate('FormScreen'), 2000)
                 }}
             />
-            <Text style={styles.text}>streamie updated!</Text>
+            <Text style={styles.text}>{streamUser} updated!</Text>
+            <FontAwesome5 style={{alignSelf:'center'}} name='wine-bottle' size={70} color='#5090ff' />
         </View>
     )
 }
@@ -21,9 +24,10 @@ SaveScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
     text: {
-        fontSize: 30,
+        fontSize: 26,
         alignSelf: 'center',
-        marginTop: 150
+        marginTop: 150,
+        marginBottom: 30
     }
 })
 
