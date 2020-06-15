dde1dc18fd46a54a3900b39df24a59865363e00f
Niels Koster <n.koster@portavita.eu>
2020-06-15 22:20:57 +0200

    add SignupScreen

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import SigninScreen from './src/screens/SigninScreen'
+import SignupScreen from './src/screens/SignupScreen'
 import FormScreen from './src/screens/FormScreen'
 import SaveScreen from './src/screens/SaveScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
 import { setNavigator } from './src/navigationRef'
 
 const stackNavigator = createStackNavigator({
-  SigninScreen, FormScreen, SaveScreen
+  SigninScreen, SignupScreen, FormScreen, SaveScreen
 })
 
 const App = createAppContainer(stackNavigator)
